e6ca5a9bc39bf24be2182745f3c4304176767f2e
beanama <129529299+beanama@users.noreply.github.com>
2023-07-26 17:31:43 +0000

    pagina de recuperacion contraseña creada

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,6 +5,7 @@ import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
+import { RecuperacionPass } from "./pages/recuperacion-password";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
@@ -27,6 +28,7 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
+                        <Route element={<RecuperacionPass />} path="/recuperacion-password" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
