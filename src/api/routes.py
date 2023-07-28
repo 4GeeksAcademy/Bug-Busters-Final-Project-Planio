753f28928c4203f885ebafd3e14d04a6070a327a
BernardoGSch-prog <127693607+BernardoGSch-prog@users.noreply.github.com>
2023-07-28 16:50:21 +0000

    log

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,47 +4,23 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask import render_template
+from flask_jwt_extended import create_access_token
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/login', methods=['POST', 'GET'])
+@api.route('/login', methods=['POST'])
 def login():
 
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    username = request.json.get('username')
+    password = request.json.get('password')
 
-    user = User.query.filter_by(username=username).first()
-    if user is None or not user.check_password(password):
+    user = User.query.filter_by(username=username, password=password).first()
+    if user is None:
         raise APIException('Invalid username or password', 401)
 
-        login_user(user)
+    token = create_access_token(identity=user.id)
     return jsonify({'message': 'Successfully logged in'}), 200
 
-
-@api.route('/signup', methods=['GET', 'POST'])
-def signup():
-
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-
-        if User.query.filter_by(username=username).first() is not None:
-            raise APIException('Username already exists', 409)
-
-        if User.query.filter_by(email=email).first() is not None:
-            raise APIException('Email already exists', 409)
-
-        user = User(username=username, email=email, password=password)
-        db.session.add(user)
-        db.session.commit()
-
-        return jsonify({'message': 'Successfully created user'}), 201
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+    return jsonify({"token": access_token, "user_id": user.id})
