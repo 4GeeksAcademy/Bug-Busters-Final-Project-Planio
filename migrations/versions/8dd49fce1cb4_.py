cedca047b859e7dae9c432d09ed28787f657b6ec
beanama <129529299+beanama@users.noreply.github.com>
2023-08-05 11:19:31 +0000

    fetch: navbar

--- migrations/versions/8dd49fce1cb4_.py
+++ migrations/versions/8dd49fce1cb4_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 3b3deda2006c
+Revision ID: 8dd49fce1cb4
 Revises: 
-Create Date: 2023-08-03 15:35:33.155881
+Create Date: 2023-08-04 17:29:28.760110
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '3b3deda2006c'
+revision = '8dd49fce1cb4'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,83 +3,69 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import planioLogo from "../../img/planio-logo.png";
 import { Avatar } from "@prismane/core";
 
-export const Navbar = ({
 
-	isPrivate,
-	isLogin,
+export const Navbar = () => {
 
-	export const Navbar = () => {
+	const location = useLocation();
+	const navigate = useNavigate()
 
-		const location = useLocation();
-		const navigate = useNavigate()
-
-		const getButtonContent = () => {
-			if (location.pathname === '/private-view') {
-				return (
-					<button className="btn btn-primary" onClick={() => {
-						localStorage.removeItem("jwt-token"); navigate("/")
-					}}>
-						Logout
-					</button>
-				);
-			}
-			return null; // Retorna null para no renderizar el botón en otras vistas
-		};
-
-		return (
-			<nav className="navbar navbar-expand-lg bg-body-tertiary">
-				<div className="container-fluid">
-					<a className="navbar-brand" href="#">
-						<img src={planioLogo} alt="Planio" width="30" height="24" />
-					</a>
-					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-							{isLogin &&
-								<>
-									<li className="nav-item">
-										<a className="nav-link active" aria-current="page" href="/login">Log in</a>
-									</li>
-									<li className="nav-item">
-										<a className="nav-link" href="/signup">Sign up</a>
-									</li>
-								</>
-							}
-							{isPrivate &&
-								<>
-									<li className="nav-item dropdown">
-										<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-											Choose a project
-										</a>
-										<ul className="dropdown-menu">
-											<li><a className="dropdown-item" href="#">Project 1</a></li>
-											<li><a className="dropdown-item" href="#">Project 2</a></li>
-											<li><a className="dropdown-item" href="#">Project 3</a></li>
-										</ul>
-									</li>
-								</>
-
-							}
-						</ul>
-
-						{isPrivate &&
-							<>
-								<form className="d-flex" role="search">
-									<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-									<button className="btn btn-outline-success" type="submit">Search</button>
-								</form>
-								<div className="d-flex align-items-center">
-									Nombre usuario
-									<Avatar color="copper" size="sm" src="https://img.freepik.com/psd-premium/avatar-personaje-dibujos-animados-lindo-masculino-3d-aislado-renderizado-3d_235528-1290.jpg">MP</Avatar>
-								</div>
-							</>
-
-						}
-					</div>
-				</div>
-				<div className="p-2">{getButtonContent()}</div>
-			</nav>
-		);
+	const getButtonContent = () => {
+		if (location.pathname === '/private-view') {
+			return (
+				<button className="btn btn-primary" onClick={() => {
+					localStorage.removeItem("jwt-token"); navigate("/")
+				}}>
+					Logout
+				</button>
+			);
+		}
+		return null; // Retorna null para no renderizar el botón en otras vistas
 	};
+
+	return (
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					<img src={planioLogo} alt="Planio" width="30" height="24" />
+				</a>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+						<li className="nav-item">
+							<a className="nav-link active" aria-current="page" href="/login">Log in</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="/signup">Sign up</a>
+						</li>
+
+						<li className="nav-item dropdown">
+							<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Choose a project
+							</a>
+							<ul className="dropdown-menu">
+								<li><a className="dropdown-item" href="#">Project 1</a></li>
+								<li><a className="dropdown-item" href="#">Project 2</a></li>
+								<li><a className="dropdown-item" href="#">Project 3</a></li>
+							</ul>
+						</li>
+
+					</ul>
+
+					<form className="d-flex" role="search">
+						<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+						<button className="btn btn-outline-success" type="submit">Search</button>
+					</form>
+					<div className="d-flex align-items-center">
+						Nombre usuario
+						<Avatar color="copper" size="sm" src="https://img.freepik.com/psd-premium/avatar-personaje-dibujos-animados-lindo-masculino-3d-aislado-renderizado-3d_235528-1290.jpg">MP</Avatar>
+					</div>
+
+				</div>
+			</div>
+			<div className="p-2">{getButtonContent()}</div>
+		</nav>
+	);
+};
