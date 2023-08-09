78a90744905ddc18103158607747e96180668590
beanama <129529299+beanama@users.noreply.github.com>
2023-08-09 18:11:38 +0000

    navbar ok

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -33,44 +33,46 @@ export const Navbar = (
 	const getButtonContent = () => {
 		if (location.pathname === '/private-view') {
 			return <>
+				<div className="navbar-collapse">
 
-				<div className="searchBar">
-					<form className="d-flex" role="search">
-						<div className="input-group">
-							<input type="text" className="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1" />
-							<span className="input-group-text" id="basic-addon1"><i className="fas fa-search" ></i></span>
-						</div>
-					</form>
-				</div>
-				<div className="projectSelector">
-					<div className="btn-group">
-						<button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-							<p>Proyecto</p>
-							<i className="btnIcon fas fa-chevron-down"></i>
-						</button>
-						<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-							<li><a className="dropdown-item" href="#">Proyecto 1</a></li>
-							<li><a className="dropdown-item" href="#">Proyecto 2</a></li>
-							<li><a className="dropdown-item" href="#">Proyecto 3</a></li>
-						</ul>
+					<div className="searchBar">
+						<form className="d-flex" role="search">
+							<div className="input-group">
+								<input type="text" className="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1" />
+								<span className="input-group-text" id="basic-addon1"><i className="fas fa-search" ></i></span>
+							</div>
+						</form>
 					</div>
-				</div>
-				<div className="notificationsIcon">
-					<Badge label="4" color="ruby" size="xs">
-						<i className="fas fa-bell"></i>
-					</Badge>
-				</div>
-				<div className="userName d-flex align-items-center">
-					<p>{userInfo.name}</p>
-					<Avatar color="copper" size="sm" src="https://img.freepik.com/psd-premium/avatar-personaje-dibujos-animados-lindo-masculino-3d-aislado-renderizado-3d_235528-1290.jpg">MP</Avatar>
-				</div>
-				<div className="logoutIcon">
-					<a onClick={() => {
-						localStorage.removeItem("jwt-token"); navigate("/")
-					}}>
-						<i className="fas fa-sign-out-alt" style={{ color: "#d70404" }}></i>
-					</a>
+					<div className="projectSelector">
+						<div className="btn-group">
+							<button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+								<p>Proyecto</p>
+								<i className="btnIcon fas fa-chevron-down"></i>
+							</button>
+							<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+								<li><a className="dropdown-item" href="#">Proyecto 1</a></li>
+								<li><a className="dropdown-item" href="#">Proyecto 2</a></li>
+								<li><a className="dropdown-item" href="#">Proyecto 3</a></li>
+							</ul>
+						</div>
+					</div>
+					<div className="notificationsIcon">
+						<Badge label="4" color="ruby" size="xs">
+							<i className="fas fa-bell"></i>
+						</Badge>
+					</div>
+					<div className="userName d-flex align-items-center">
+						<p>{userInfo.name}</p>
+						<Avatar color="copper" size="sm" src="https://img.freepik.com/psd-premium/avatar-personaje-dibujos-animados-lindo-masculino-3d-aislado-renderizado-3d_235528-1290.jpg">MP</Avatar>
+					</div>
+					<div className="logoutIcon">
+						<a onClick={() => {
+							localStorage.removeItem("jwt-token"); navigate("/")
+						}}>
+							<i className="fas fa-sign-out-alt" style={{ color: "#d70404" }}></i>
+						</a>
 
+					</div>
 				</div>
 
 
@@ -79,8 +81,8 @@ export const Navbar = (
 		return <>
 
 			<div className="loginOptions navbar-nav me-auto mb-2 mb-lg-0">
-				<a className="nav-link" href="/login">Log in</a>
-				<a className="nav-link" href="/signup">Sign up</a>
+				<a className="nav-link mr-auto" href="/login">Log in</a>
+				<a className="nav-link mr-auto" href="/signup">Sign up</a>
 			</div>
 
 
@@ -88,12 +90,12 @@ export const Navbar = (
 	};
 
 	return (
-		<nav className="navbar justify-content-end navbar-expand-lg bg-body-tertiary">
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
 			<div className="container-fluid">
 				<a className="navbar-brand" href="#">
 					<img src={planioLogo} alt="Planio" />
 				</a>
-				<div className="collapse navbar-collapse ">
+				<div className="container-fluid">
 					{getButtonContent()}
 				</div>
 			</div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import planioImg1 from "../../img/planio-home-1.jpg";
-import planioImg2 from "../../img/planio-home-2.jpg";
-import planioImg3 from "../../img/planio-home-3.jpg";
-import planioImg4 from "../../img/planio-home-4.jpg";
-import planioImg5 from "../../img/planio-home-5.jpg";
+import planioImg1 from "../../img/planio-home-1.png";
+import planioImg2 from "../../img/planio-home-2.png";
+import planioImg3 from "../../img/planio-home-3.png";
+import planioImg4 from "../../img/planio-home-4.png";
+import planioImg5 from "../../img/planio-home-5.png";
 import { Link, useNavigate } from "react-router-dom";
 import "../../styles/home.css";
 
@@ -24,12 +24,12 @@ export const Home = () => {
 				<span>Sign up here!</span>
 			</Link>
 			<div className="container fade-in-img">
-				<div className="row align-items-end">
+				<div className="row align-items-end mt-4">
 					<div className="col">
-						<img src={planioImg1} alt="..." />
+						<img src={planioImg1} className="img-fluid" alt="..." />
 					</div>
 					<div className="col">
-						<img src={planioImg2} alt="..." />
+						<img src={planioImg2} className="img-fluid" alt="..." />
 					</div>
 					<div className="col">
 						<img src={planioImg3} className="img-fluid" alt="..." />
