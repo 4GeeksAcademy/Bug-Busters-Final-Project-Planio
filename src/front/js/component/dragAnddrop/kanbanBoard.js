b624a127f939ba50dd52842f57bfec80df203861
Pedro Luis Alvarado Herrera <129191234+pedroalvaradoh@users.noreply.github.com>
2023-08-23 16:43:25 +0000

    feat: drag and drop feature in progress 2

--- src/front/js/component/dragAnddrop/kanbanBoard.js
+++ src/front/js/component/dragAnddrop/kanbanBoard.js
@@ -36,20 +36,26 @@ export const KanbanBoard = () => {
         } else {
             actions.getUserInfo()
                 .then((userInfo) => {
+                    const foundProject = userInfo?.projects?.find(project => project.id === parsedProjectId);
+                    setProject(foundProject);
+
+                    return foundProject;
+                })
+                .then((foundProject) => {
+                    setTodoList(foundProject?.tasks);
                     setLoading(false);
                 })
-                // .then(()=>{
-
-                // })
                 .catch((error) => {
                     console.error(error);
                 });
         }
     }, [validated_token, updatedComponent]);
 
-    const userInfo = store.user_info[0];
 
-    const project = userInfo?.projects?.find(project => project.id === parsedProjectId);
+
+    const userInfo = store.user_info[0];
+    const [project, setProject] = useState(null);
+
 
     if (loading) {
         return <div className="d-flex justify-content-center align-items-center vh-100">
@@ -63,14 +69,46 @@ export const KanbanBoard = () => {
         return null;
     }
 
+
+
     const handleDragEnd = (result) => {
-        // const { destination, source, draggableId } = result;
+        const { destination, source, draggableId } = result;
 
-        // if (source.droppableId == destination.droppableId) return;
+        if (!destination) {
+            return;
+        }
+
+        if (destination.droppableId === source.droppableId && destination.index === source.index) {
+            return;
+        }
+
+
+        const draggedTask = project?.tasks.find(task => task.id.toString() === draggableId);
+
+        if (!draggedTask) {
+            return;
+        }
+
+        if (destination.droppableId === "1") {
+            const updatedInProgressList = inProgressList.filter(task => task.id !== draggedTask.id);
+            setInProgressList(updatedInProgressList);
+
+            setTodoList([...todoList, draggedTask]);
+        } else if (destination.droppableId === "2") {
+            const updatedTodoList = todoList.filter(task => task.id !== draggedTask.id);
+            setTodoList(updatedTodoList);
+
+            setInProgressList([...inProgressList, draggedTask]);
+        } else if (destination.droppableId === "3") {
+            const updatedDoneList = doneList.filter(task => task.id !== draggedTask.id);
+            setDoneList(updatedDoneList);
+
+            setInProgressList([...inProgressList, draggedTask]);
+        }
+    };
 
 
 
-    }
 
     if (validated_token) {
 
@@ -82,11 +120,14 @@ export const KanbanBoard = () => {
                             {project ? <h1>{project?.title}</h1> : <p>Project not found or doesn't exist</p>}
                         </div>
                         <div className="row justify-content-between">
-                            <div className="col simple-card my-4 p-4 overflow-hidden text-break">
-                                <Column title={"To-do"} projectId={project?.id} username={userInfo?.username} projectTasks={project?.tasks} id={"1s"} />
+                            <div className="col-6 simple-card my-4 p-4 overflow-hidden text-break">
+                                <Column title={"To-do"} projectTasks={todoList} id={"1"} />
                             </div>
-                            <div className="col simple-card my-4 p-4 overflow-hidden text-break">
-                                <Column title={"In Progress"} projectId={project?.id} username={userInfo?.username} projectTasks={project?.tasks} id={"1s"} />
+                            <div className="col-6 simple-card my-4 p-4 overflow-hidden text-break">
+                                <Column title={"In Progress"} projectTasks={inProgressList} id={"2"} />
+                            </div>
+                            <div className="col-6 simple-card my-4 p-4 overflow-hidden text-break">
+                                <Column title={"Done"} projectTasks={doneList} id={"3"} />
                             </div>
                         </div>
                     </div>
